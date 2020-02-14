d7a0756c2e143a8a7d7126e012da209dd308bd9c
thaihieuhuynh1752 <thaihieuhuynh1752@gmail.com>
2020-02-14 11:04:12 +0700

    assignment 2

--- src/components/MainComponent.js
+++ src/components/MainComponent.js
@@ -4,6 +4,7 @@ import Menu from './MenuComponent';
 import Footer from './FooterComponent';
 import Contact from './ContactComponent';
 import Home from './HomeComponent';
+import About from './AboutComponent';
 import DishDetail from './DishdetailComponent';
 import {COMMENTS} from '../shared/comments';
 import {PROMOTIONS} from '../shared/promotions';
@@ -36,12 +37,22 @@ class Main extends Component {
       )
     }
 
+    const DishWithId=({match})=>{
+      return(
+        <DishDetail dish={this.state.dishes.filter((dish)=>dish.id===parseInt(match.params.dishId,10))[0]}
+          comments={this.state.comments.filter((comment)=>comment.dishId===parseInt(match.params.dishId,10))}
+        />
+      )
+    }
+
     return (
       <div>
         <Header/>
         <Switch>
           <Route path="/home" component={HomePage}/>
           <Route exact path="/menu" component={()=><Menu dishes={this.state.dishes}/>}/>
+          <Route path="/menu/:dishId" component={DishWithId}/>
+          <Route path="/aboutus" component={()=><About leaders={this.state.leaders}/>}/>
           <Route exact path="/contactus" component={Contact}/>
           <Redirect to="/home"/>
         </Switch>
